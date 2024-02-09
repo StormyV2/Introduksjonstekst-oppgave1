8c0dbfddca052a49aaff5fe6d6bc9e471fccb58a
StormyV2 <Stormyyv77@gmail.com>
2024-02-09 13:28:58 +0100

    update2
    added close buton and hover mouse on 2 buttons

--- java.js
+++ java.js
@@ -1,13 +1,20 @@
 function amandoinfo(){
     document.getElementById('amandoned').innerHTML= /*HTML*/`
-    <div class="amandocard">
-   <ul>Navn: Amando Ball</ul>
-    <ul>Location: Bergen</ul>
-   <ul> Alder: 19</ul>
-    <ul>Hobbyer: Styrke trening, Basketball, Gaming, Henge med venner, Alkoholiker i helgene</ul>
-    <ul>Games: CS:GO, War Thunder, League Of Legends
+    <div id="closeinfo">
+      <div class="amandocard">
+         <ul>Navn: Amando Ball</ul>
+         <ul>Location: Bergen</ul>
+         <ul> Alder: 19</ul>
+         <ul>Hobbyer: Styrke trening, Basketball, Gaming, Henge med venner, Alkoholiker i helgene</ul>
+         <ul>Games: CS:GO, War Thunder, League Of Legend
+         <div class="closer" onclick="closeinfo()">Lukk</div>
     </div>
 `
+}
+
+function closeinfo(){
+    document.getElementById('closeinfo').innerHTML=``;
+    
 }
 
 function byttbak(){
